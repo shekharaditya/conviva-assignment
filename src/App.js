aee95597cb4e15e984909c611ae29a4297d7f9b2
Aditya Shekhar <aditya.shekhar@broadcom.com>
2020-07-19 19:34:36 +0530

    minor style changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import CustomerTable from './components/CustomerTable';
 import './App.css';
+
 
 class App extends Component {
   constructor(props) {
@@ -23,6 +27,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Typography variant="h6" color="inherit">
+              Customers Info-: (Click on customer row to toggle the address associated with them)
+            </Typography>
+          </Toolbar>
+        </AppBar>
         <CustomerTable customers={this.state.customers} />
       </div>
     );
--- src/components/style.js
+++ src/components/style.js
@@ -1,31 +1,34 @@
 const styles = () => ({
-    infoHeader: {
-      background: 'linear-gradient(45deg, #7AD7F0 30%, #DBF3FA 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-    },
-    addressHeader: {
-      background: 'lightgrey',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-    },
-    addressTable: {
-      display: 'none',
-    },
-    noAddress: {
-      display: 'none',
-    },
-    infoRow: {
-      background: 'lightgrey',
-    }
+  infoTable: {
+    marginTop: '10px',
+  },
+  infoHeader: {
+    background: 'linear-gradient(45deg, #7AD7F0 30%, #DBF3FA 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+  addressHeader: {
+    background: 'lightgrey',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+  addressTable: {
+    display: 'none',
+  },
+  noAddress: {
+    display: 'none',
+  },
+  infoRow: {
+    background: 'lightgrey',
+  }
   
 });
 
